35ae21ac098ea56fceb513713d89037ee058219f
Niyiadekola <niyiadekola04@gmail.com>
2022-10-18 12:33:55 +0100

    Update get_size.c

--- get_size.c
+++ get_size.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
 /**
+ * include main file
+ */
+
+/**
  * get_size - Calculates the size to cast the argument
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
