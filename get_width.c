3a8633054a3dd211cdbcbd70a6ea446bef999169
Niyiadekola <niyiadekola04@gmail.com>
2022-10-18 12:34:10 +0100

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
 /**
+ * include main file
+ */
+
+/**
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
